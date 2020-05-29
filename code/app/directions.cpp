f299cdb324b26c15d0bfa922d4a4fe35cbf93124
Makar Baderko <makarbaderko@gmail.com>
2020-05-29 19:45:19 +0300

    Added Directions of Studying

--- code/app/directions.cpp
+++ code/app/directions.cpp
@@ -1,4 +1,13 @@
 #include "directions.h"
-
 Directions::Directions(){
+    //Группы направлений
+    WebProgramDirs = {"HTML & CSS", "JavaScript", "Node.js", "C#", "Python"};
+    VideoAndMusicDirs = {"After Effects", "Premier Pro", "Создание музыки"};
+    Creative2dDirs = {"Photoshop", "Illustrator"};
+    Creative3dDirs = {"Sculptris", "3D Zbrush", "3D TincerCAD"};
+    OtherProgrammingDirs = {"Информационная безопасность"};
+    GameCreationDirs = {"GameMaker", "ROBLOX", "Unity"};
+    BasicsDirs = {"Компьютерная грамотность", "Minecraft", "App Inventor"};
+    AppCreationdDirs = {"App Inventor", "JavaScript", "C#", "C++"};
+    SportProgrammingDirs = {"Олимпиадные задачи", "Алгоритмы", "Математика"};
 }
--- code/app/directions.h
+++ code/app/directions.h
@@ -1,11 +1,21 @@
 #ifndef DIRECTIONS_H
 #define DIRECTIONS_H
-
-
+#include <vector>
+#include <QString>
 class Directions
 {
 public:
+    std::vector<QString> WebProgramDirs;
+    std::vector<QString> Creative3dDirs;
+    std::vector<QString> Creative2dDirs;
+    std::vector<QString> VideoAndMusicDirs;
+    std::vector<QString> OtherProgrammingDirs;
+    std::vector<QString> GameCreationDirs;
+    std::vector<QString> BasicsDirs;
+    std::vector<QString> AppCreationdDirs;
+    std::vector<QString> SportProgrammingDirs;
     Directions();
+
 };
 
 #endif // DIRECTIONS_H
