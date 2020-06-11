25edde4e9bdb356530b3d8e2cf960132490d324f
E2-Tech <raskindjoshua@gmail.com>
2020-06-11 13:46:55 +0200

    Rewrote json class

--- code/app/json.h
+++ code/app/json.h
@@ -1,11 +1,21 @@
 #ifndef JSON_H
 #define JSON_H
 
+#include <QVariantMap>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+#include <QFile>
 
 class Json
 {
+private:
+   QJsonObject json;
+
 public:
     Json();
+    bool create_json();
 };
 
 #endif // JSON_H
--- code/app/main.cpp
+++ code/app/main.cpp
@@ -1,4 +1,5 @@
 #include "widget.h"
+#include "json.h"
 
 #include <QApplication>
 
@@ -6,6 +7,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+
+    Json json;
+
+    if(!json.create_json())
+        qDebug("Failed to create json file");
+
+
+
+
     w.show();
     return a.exec();
 }
